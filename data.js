346b680642995cb84573ae22dc9b6f18e1d1e010
Tim T <tim.email.personal@gmail.com>
2023-01-11 07:05:21 -1000

    Add data file

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    id: 1, title: 'title 1', description: 'This is a description. This is a task that needs to be done', date: '2018-07-22', status: 'backlog', url: 'https://httpstatusdogs.com/img/200.jpg', asignees: ["John"]
+  },
+  {
+    id: 2, title: 'title 2', description: 'This is a description. This is a task that needs to be done', date: '2018-07-22', status: 'on deck', url: 'https://httpstatusdogs.com/img/200.jpg', asignees: ["John", "Taylor"]
+  },
+  {
+    id: 3, title: 'title 3', description: 'This is a description. This is a task that needs to be done', date: '2018-07-22', status: 'in progress', url: 'https://httpstatusdogs.com/img/200.jpg', asignees: ["John", "Taylor"]
+  },
+  {
+    id: 4, title: 'title 4', description: 'This is a description. This is a task that needs to be done', date: '2018-07-22', status: 'complete', url: 'https://httpstatusdogs.com/img/200.jpg', asignees: ["Taylor"]
+  },
+  {
+    id: 5, title: 'title 5', description: 'This is a description. This is a task that needs to be done', date: '2018-07-22', status: 'backlog', url: 'https://httpstatusdogs.com/img/200.jpg', asignees: ["John", "Taylor"]
+  },
+  {
+    id: 6, title: 'title 6', description: 'This is a description. This is a task that needs to be done', date: '2018-07-22', status: 'backlog', url: 'https://httpstatusdogs.com/img/200.jpg', asignees: ["Taylor"]
+  }
+]
+
+module.exports = data
