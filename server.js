f3429577a002b0cbc03a271acdbb06e1a379e15e
Tim T <tim.email.personal@gmail.com>
2023-01-19 08:55:40 -1000

    Save changes

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/todos', (request, response) => {
 })
 
 app.get('/todos/:id', (request, response) => {
-  const id = parseInt(request.params.id)
+  const id = request.params.id
   const foundTodo = app.locals.todos.find(idea => idea.id === id)
   if (!foundTodo) {
     return response.status(404).json({ message: `Sorry, no todo found with an id of ${id}` })
@@ -41,19 +41,22 @@ app.post('/todos', (request, response) => {
 })
 
 app.delete('/todos/:id', (request, response) => {
-  const id = parseInt(request.params.id)
+  const id = request.params.id
   const filteredTodo = app.locals.todos.filter(todo => String(todo.id) !== String(id))
   app.locals.todos = filteredTodo
   response.status(200).json(app.locals.todos)
 })
 
 app.put('/todos/:id', (req, res) => {
-  const { id } = req.params
+  const { id } = req.params.id
+  console.log("PUT ID", id)
   const { content, date, destination } = req.body
-  const updatedTask = app.locals.todos.find(todo => todo.id == id)
+  const updatedTask = app.locals.todos.find(todo => String(todo.id) == String(id))
+  console.log("PUT TASK BEFORE", updatedTask)
   updatedTask.destination  = destination
   updatedTask.date = date 
   updatedTask.content = content 
+  console.log("PUT TASK AFTER", updatedTask)
   return res.json(updatedTask)
 })
 
